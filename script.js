a89f4a2d3efdca7d3ae710320efd80992800ec44
KAITERRR <143228794+KAITERRR@users.noreply.github.com>
2023-08-26 17:41:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const downloadButton = document.getElementById("downloadButton");
-const videoUrlInput = document.getElementById("videoUrl");
-const thumbnailContainer = document.getElementById("thumbnailContainer");
-
-downloadButton.addEventListener("click", async () => {
-    const videoUrl = videoUrlInput.value;
-    const videoId = videoUrl.split("v=")[1];
-    const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
-
-    const thumbnailImage = document.createElement("img");
-    thumbnailImage.src = thumbnailUrl;
-
-    thumbnailContainer.innerHTML = "";
-    thumbnailContainer.appendChild(thumbnailImage);
-});
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>YouTube Thumbnail Downloader</title>
+</head>
+<body>
+    <header>
+        <h1>YouTube Thumbnail Downloader</h1>
+    </header>
+    <main>
+        <div class="thumbnail-container">
+            <input type="text" id="videoUrl" placeholder="Enter YouTube Video URL">
+            <button id="downloadButton">Download Thumbnail</button>
+        </div>
+        <div class="thumbnail-preview">
+            <h2>Thumbnail Preview</h2>
+            <img class="thumbnail-image" id="thumbnailImage" src="" alt="Downloaded Thumbnail">
+        </div>
+    </main>
+    <footer>
+        <p>&copy; 2023 YouTube Thumbnail Downloader. All rights reserved.</p>
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
